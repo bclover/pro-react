19fce6c55afe183b7915904d73fd67df266b6273
Bryan Clover <bclover09@gmail.com>
2016-10-21 11:11:59 -0400

    Added styles and fixed bugs.

--- chapter1/app/Card.js
+++ chapter1/app/Card.js
@@ -8,7 +8,7 @@ class Card extends Component {
         <div className="card_title">{this.props.title}</div>
         <div className="card_details">
         {this.props.description}
-        <CheckList cardId={this.props.id} tasks={this.props.tasks} />
+        <CheckList key={this.props.id} cardId={this.props.id} tasks={this.props.tasks} />
         </div>
       </div>
     );
