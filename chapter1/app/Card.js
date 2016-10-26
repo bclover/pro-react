1dbf03a4a59c695e02384de2af5f4f301a3707d2
Bryan Clover <bclover09@gmail.com>
2016-10-26 13:42:58 -0400

    Improved card toggle code

--- chapter1/app/Card.js
+++ chapter1/app/Card.js
@@ -10,6 +10,10 @@ class Card extends Component {
     };
   }
 
+  toggleDetails() {
+    this.setState({showDetails: !this.state.showDetails});
+  }
+
   render() {
     let cardDetails;
     if (this.state.showDetails){
@@ -23,8 +27,9 @@ class Card extends Component {
 
     return (
       <div className='card'>
-        <div className='card_title'
-	  onClick={()=>this.setState({showDetails: !this.state.showDetails})}>{this.props.title}</div>
+        <div className='card_title' onClick={this.toggleDetails.bind(this)}>
+	  {this.props.title}
+	</div>
 	{cardDetails}
       </div>
     );
